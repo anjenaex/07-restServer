9eff10574f21d6d9ca8bc09b448d779651fec668
anjenaex <anjenaex@gmail.com>
2018-12-07 12:39:27 +0100

    intento de heroku

--- server/server.js
+++ server/server.js
@@ -38,5 +38,5 @@ app.delete('/usuario', function (req, res) {
 });
 
 app.listen(process.env.PORT, () => {
-  console.log(`Escuchando en el puerto ${process.env.PORT}`);
+  console.log('Escuchando en el puerto:'process.env.PORT);
 });
