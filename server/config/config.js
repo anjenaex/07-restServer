422336b0c9333f4aa69066a693700f08b817874d
anjenaex <anjenaex@gmail.com>
2018-12-09 23:54:30 +0100

    RestSever - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,24 @@
 // ====================
 
 process.env.PORT = process.env.PORT || 3000
+
+// ====================
+// Entorno
+// ====================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// ====================
+// Base de datos
+// ====================
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+  urlDB = 'mongodb://localhost/cafe';
+} else {
+  urlDB = 'mongodb://cafe-user:abc123@ds129904.mlab.com:29904/cafe';
+}
+process.env.URLDB = urlDB;
+
+
+// mongodb://localhost/cafe
+// mongodb://<dbuser>:<dbpassword>@ds129904.mlab.com:29904/cafe
